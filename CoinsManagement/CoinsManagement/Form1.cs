297771961d22d649ba29f9149e16173a9c408c93
4201104150 <shenlong436@gmail.com>
2019-05-11 23:14:35 +0700

    function get data coins

--- CoinsManagement/CoinsManagement/Form1.cs
+++ CoinsManagement/CoinsManagement/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Web;
 
 namespace CoinsManagement
 {
@@ -16,6 +18,29 @@ namespace CoinsManagement
         {
             InitializeComponent();
         }
+
+        private static string API_KEY = "0c1437b5-12b4-4f1d-90e1-83a8eefc892f";
+        /// <summary>
+        /// function get data coins
+        /// </summary>
+        static string makeAPICall()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = "5000";
+            queryString["convert"] = "USD";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            return client.DownloadString(URL.ToString());
+
+        }
+        
 
         private void btnLoc_Click(object sender, EventArgs e)
         {
